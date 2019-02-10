8a8bff62b7dd1aa01046acdee7707a234ae546c8
shrilakshmishastry <shrilakshmishastry@gmail.com>
2019-02-10 19:49:14 +0530

    Added few new questions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# chatbox_nlp
-This is a small project of chatbot.Depended on nltk library.
+#chatbox
+This is a chatbot project .
+I have  used nltk library.
+For a set of questions chatbox answers.
+
+### Prerequisites
+
+  1.NLTK
+  2.python3 or Python2
+
+### Running
+
+  python3 <filename>
+
+
+
+## Built With
+
+* [NLTK](https://www.nltk.org/) - NLTK is a leading platform for building Python programs to work with human language data.
--- chat_box.py
+++ chat_box.py
@@ -5,19 +5,25 @@ pairs = [
     ["You went to Shimoga",]
 ],
 [
+    r"Hii I am (.*)",
+    ["Hello %1 How are you doing today??" ],
+],
+[
+    r"How are you doing??",
+    ["I am doing Good,Thank you"],
+],
+[
     r"quit",
     ["Bye take care"]
 ],
 ]
-print(reflections)
-
 myreflection ={
 "go":"went"
 }
 
 def chatty():
-    print("Hi I am chatbox type quit exit")
-    chat = Chat(pairs)
+    print("Hi I am Chatty,You can chat with me.Say quit to exit")
+    chat = Chat(pairs,reflections)
     chat.converse()
 
 if __name__ == "__main__"    :
